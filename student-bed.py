44eaaf9832b3766351ecfb28bd57d251efc6b58e
Paul Kussmaul <Paul.Kussmaul@mailbox.org>
2023-04-10 09:51:23 +0200

    jamb redesign

--- student-bed.py
+++ student-bed.py
@@ -28,9 +28,9 @@ def echo(message) -> None:
 # all measures are in mm
 
 measures ={
-    "@thickness.slim":18,
-    "@thickness.plumb":27,
-    "@side.length":68,
+    "@thickness.1":18,
+    "@thickness.2":27,
+    "@side.length":120,
     "mattress.height":180,
     "mattress.width":900,
     "mattress.length":2100,
@@ -42,22 +42,38 @@ measures ={
 
 # derived measures
 
-measures["stringer.height"] = measures["@thickness.plumb"]
-measures["stringer.width"]  = measures["@side.length"]
-measures["stringer.length"] = measures["mattress.length"]*0.5
-measures["ledger.height"]   = measures["@thickness.plumb"]
-measures["ledger.width"]    = measures["@side.length"]
-measures["ledger.length"]   = measures["mattress.width"]-2*measures["stringer.width"]
-measures["batten.height"]   = measures["@thickness.slim"]
-measures["batten.width"]    = measures["@side.length"]
-measures["batten.length"]   = measures["mattress.width"]
-measures["batten.gap"]      = (measures["stringer.length"]-9*measures["batten.width"])/8
-measures["jamb.side"]       = measures["@side.length"]
-measures["jamb.length"]     = measures["mattress.altitude"]-measures["@thickness.slim"]-measures["@thickness.plumb"]
-measures["angle.depth"]     = measures["@side.length"]
-measures["angle.limb"]      = measures["@side.length"]*2.5
-measures["angle.thickness"] = measures["@thickness.slim"]
-measures["angle.chamfer"]   = measures["angle.limb"]-measures["angle.thickness"]
+measures["@width.1"]         = measures["@side.length"]*0.5
+measures["@width.2"]         = measures["@side.length"]*2/3
+measures["@width.3"]         = measures["@side.length"]
+
+measures["stringer.height"]  = measures["@thickness.2"]
+measures["stringer.width"]   = measures["@width.3"]
+measures["stringer.length"]  = measures["mattress.length"]*0.5
+
+measures["ledger.height"]    = measures["@thickness.2"]
+measures["ledger.width"]     = measures["@width.3"]
+measures["ledger.length"]    = measures["mattress.width"]-2*measures["@width.3"]
+
+measures["batten.height"]    = measures["@thickness.1"]
+measures["batten.width.1"]   = measures["@width.1"]
+measures["batten.width.2"]   = measures["@width.2"]
+measures["batten.width.3"]   = measures["@width.3"]
+measures["batten.length"]    = measures["mattress.width"]
+measures["batten.gap"]       = (measures["stringer.length"]-measures["batten.width.3"]-5*measures["batten.width.2"]-measures["batten.width.1"])/6
+
+measures["cutoff.clearance"] = 0.2
+measures["cutoff.thickness"] = measures["@thickness.1"]+measures["cutoff.clearance"]
+measures["cutoff.width"]     = measures["@width.2"]
+measures["cutoff.length"]    = (measures["@width.2"]+measures["cutoff.clearance"])*2
+
+measures["jamb.thickness"]   = measures["@thickness.2"]
+measures["jamb.side"]        = measures["@side.length"]
+measures["jamb.length"]      = measures["mattress.altitude"]-measures["@thickness.1"]-measures["@thickness.2"]
+
+measures["rib.thickness"]    = measures["@thickness.2"]
+measures["rib.width"]        = measures["@width.2"]
+measures["rib.length.1"]     = measures["ledger.length"]+2*(measures["@thickness.2"]+15)
+measures["rib.length.2"]     = measures["stringer.length"]-1.5*measures["@side.length"]+2*(measures["@thickness.2"]+15)
 
 # model class
 
@@ -91,36 +107,49 @@ class SimpleBed:
         ledger = cq.Workplane("XY").box(lw,ll,lh)
         #show_object(ledger,name="ledger",options={"alpha":0.2,"color":(255,170,0)})
 
-        bh = self.measures["batten.height"]
-        bw = self.measures["batten.width"]
-        bl = self.measures["batten.length"]
-        batten = cq.Workplane("XY").box(bw,bl,bh)
-        #show_object(batten,name="batten",options={"alpha":0.2,"color":(255,170,0)})
+        bh   = self.measures["batten.height"]
+        bw_1 = self.measures["batten.width.1"]
+        bw_2 = self.measures["batten.width.2"]
+        bw_3 = self.measures["batten.width.3"]
+        bl   = self.measures["batten.length"]
+        batten_1 = cq.Workplane("XY").box(bw_1,bl,bh)     # half  (only for 3D-printing)
+        batten_2 = cq.Workplane("XY").box(bw_2,bl,bh)     # small (middle)
+        batten_3 = cq.Workplane("XY").box(bw_3,bl,bh)     # large (outwards)
+        #show_object(batten_3,name="batten",options={"alpha":0.2,"color":(255,170,0)})
 
+        jt = self.measures["jamb.thickness"]
         js = self.measures["jamb.side"]
         jl = self.measures["jamb.length"]
-        jamb = cq.Workplane("XY").box(js,js,jl)
-        #show_object(jamb,name="jamb",options={"alpha":0.2,"color":(255,170,0)})
+        ct = self.measures["cutoff.thickness"]
+        cw = self.measures["cutoff.width"]
+        cl = self.measures["cutoff.length"]
+        cxp = cq.Workplane("XY").box(cw,ct,cl).translate((+0.5*js,0,0.5*jl))      # cutout x+
+        cxm = cq.Workplane("XY").box(cw,ct,cl).translate((-0.5*js,0,0.5*jl))      # cutout x-
+        cyp = cq.Workplane("XY").box(ct,cw,cl).translate((0,+0.5*js,0.5*jl))      # cutout y+
+        jamb   = cq.Workplane("XY").box(js,js,jl).faces("<Z or >Z").shell(-jt)    # shell
+        jamb_corner = jamb.cut(cxp).cut(cyp)
+        jamb_middle = jamb_corner.cut(cxm)
+        #show_object(cx,name="cutoff",options={"alpha":0.2,"color":(255,170,0)})
+        #show_object(jamb_corner,name="jamb",options={"alpha":0.2,"color":(255,170,0)})
 
-        ad = self.measures["angle.depth"]
-        al = self.measures["angle.limb"]
-        at = self.measures["angle.thickness"]
-        ac = self.measures["angle.chamfer"]
-        angle = cq.Workplane("XY").box(al,ad,al,centered=False)
-        angle = angle.faces(">X or >Y or <Z").shell(-at)
-        angle = angle.edges(">X and <Z").chamfer(ac)
-        show_object(angle,name="angle",options={"alpha":0.2,"color":(255,170,0)})
+        rt   = self.measures["rib.thickness"]
+        rw   = self.measures["rib.width"]
+        rl_1 = self.measures["rib.length.1"]
+        rl_2 = self.measures["rib.length.2"]
+        rib_ledger   = cq.Workplane("XY").box(rt,rl_1,rw)          # rib for ledger
+        rib_stringer = cq.Workplane("XY").box(rl_2,rt,rw)          # rib for stringer
+        show_object(rib_ledger,name="rib",options={"alpha":0.2,"color":(255,170,0)})    
 
         stringer_moved = stringer.translate((600,0,250))
         ledger_moved   = ledger.translate((0,500,250))
-        batten_moved   = batten.translate((0,500,350))
+        batten_moved   = batten_3.translate((0,500,350))
         parts = jamb.union(stringer_moved).union(ledger_moved).union(batten_moved)
         #show_object(parts,name="parts",options={"alpha":0.2,"color":(255,170,0)})
 
         zo = 0.5*(self.measures["stringer.height"]+self.measures["batten.height"])
         yo = 0.5*(self.measures["mattress.width"]-self.measures["stringer.width"])
-        xo = 0.5*(self.measures["stringer.length"]-self.measures["batten.width"])
-        skew = self.measures["batten.width"]+self.measures["batten.gap"]
+        xo = 0.5*(self.measures["stringer.length"]-self.measures["batten.width.2"])
+        skew = self.measures["batten.width.2"]+self.measures["batten.gap"]
         lift = 0.5*self.measures["stringer.height"]+self.measures["jamb.length"]
         stringer_front = stringer.translate((0,+yo,0))
         stringer_back  = stringer.translate((0,-yo,0))
@@ -129,15 +158,15 @@ class SimpleBed:
         duckboard = stringer_front.union(stringer_back)
         duckboard = duckboard.union(ledger_left)
         duckboard = duckboard.union(ledger_right)
-        duckboard = duckboard.union(batten.translate((0,0,zo)))        # middle
-        duckboard = duckboard.union(batten.translate((-xo,0,zo)))      # left most
-        duckboard = duckboard.union(batten.translate((+xo,0,zo)))      # right most
-        duckboard = duckboard.union(batten.translate((-1*skew,0,zo)))  # 1 skew left
-        duckboard = duckboard.union(batten.translate((+1*skew,0,zo)))  # 1 skew right
-        duckboard = duckboard.union(batten.translate((-2*skew,0,zo)))  # 2 skew left
-        duckboard = duckboard.union(batten.translate((+2*skew,0,zo)))  # 2 Skew right
-        duckboard = duckboard.union(batten.translate((-3*skew,0,zo)))  # 3 skew left
-        duckboard = duckboard.union(batten.translate((+3*skew,0,zo)))  # 3 skew right
+        duckboard = duckboard.union(batten_3.translate((0,0,zo)))        # middle
+        duckboard = duckboard.union(batten_3.translate((-xo,0,zo)))      # left most
+        duckboard = duckboard.union(batten_3.translate((+xo,0,zo)))      # right most
+        duckboard = duckboard.union(batten_3.translate((-1*skew,0,zo)))  # 1 skew left
+        duckboard = duckboard.union(batten_3.translate((+1*skew,0,zo)))  # 1 skew right
+        duckboard = duckboard.union(batten_3.translate((-2*skew,0,zo)))  # 2 skew left
+        duckboard = duckboard.union(batten_3.translate((+2*skew,0,zo)))  # 2 Skew right
+        duckboard = duckboard.union(batten_3.translate((-3*skew,0,zo)))  # 3 skew left
+        duckboard = duckboard.union(batten_3.translate((+3*skew,0,zo)))  # 3 skew right
         duckboard = duckboard.translate((0,0,lift))
         #show_object(duckboard,name="duckbboard",options={"alpha":0.2,"color":(255,170,0)})
 
